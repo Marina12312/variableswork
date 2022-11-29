0f843db73f5ff1898cc8e1591d855ae25c7ce405
Admin <kislovv95@mail.ru>
2022-11-29 16:22:45 +0300

    задача 7

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,12 @@ public class Main {
         var weightDifference = boxer2 - boxer1;
         System.out.println("Боксер2 на "+ weightDifference+ " кг больше баксера1");
 
+        var difference = boxer1 - boxer2;
+        System.out.println("Боксер1 на " + difference+ " кг меньше боксера2");
+
+        var overload = boxer2 % boxer1;
+        System.out.println("боксер1 на " + overload + "кг меньше боксера2");
+
 
 
     }
